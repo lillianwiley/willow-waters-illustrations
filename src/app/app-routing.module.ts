d5f8d350a46d8dbba80df93df43b03a3724d2cca
Lillian Wiley <lillianwiley92@gmail.com>
2021-10-24 11:34:13 -0400

    feature/ww-1 basic structure done with routing. no css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ShopComponent } from './shop/shop.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home', component: AppComponent},
+  {path: 'about', component: AboutMeComponent},
+  {path: 'shop', component: ShopComponent},
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'contact', component: ContactComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [
+
+  AppComponent,
+  AboutMeComponent,
+  ShopComponent,
+  GalleryComponent,
+  ContactComponent
+
+]
